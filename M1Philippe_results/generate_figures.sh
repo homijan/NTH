764d0e658f80e86db1be3855ff0aaafa14b741e4
milan_holec <homijan@gmail.com>
2017-12-13 16:05:53 +0100

    Added GLVIS scripts to generate M1Phillipe tests output placed in docs/figs folder.

---
+++ M1Philippe_results/generate_figures.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+# problem 4 in 1D
+# generate figs locally
+cd resultsp41D1e6
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp41D1e5
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp41D1e4
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp41D1e3
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp41D1e2
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ..
+# copy figs
+cp resultsp41D1e6/density.png ../docs/figs/density_p41D.png
+cp resultsp41D1e6/temperature.png ../docs/figs/temperature_p41D.png
+cp resultsp41D1e6/Kn.png ../docs/figs/Kn_p41D1e6.png
+cp resultsp41D1e6/hflux.png ../docs/figs/hflux_p41D1e6.png
+cp resultsp41D1e5/Kn.png ../docs/figs/Kn_p41D1e5.png
+cp resultsp41D1e5/hflux.png ../docs/figs/hflux_p41D1e5.png
+cp resultsp41D1e4/Kn.png ../docs/figs/Kn_p41D1e4.png
+cp resultsp41D1e4/hflux.png ../docs/figs/hflux_p41D1e4.png
+cp resultsp41D1e3/Kn.png ../docs/figs/Kn_p41D1e3.png
+cp resultsp41D1e3/hflux.png ../docs/figs/hflux_p41D1e3.png
+cp resultsp41D1e2/Kn.png ../docs/figs/Kn_p41D1e2.png
+cp resultsp41D1e2/hflux.png ../docs/figs/hflux_p41D1e2.png
+# problem 5 in 1D
+# generate figs locally
+cd resultsp51D1e14
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D1e13
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D1e12
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D1e11
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D1e10
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D1e9
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D1e8
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp51D5e7
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ..
+# copy figs
+cp resultsp51D1e14/density.png ../docs/figs/density_p51D.png
+cp resultsp51D1e14/temperature.png ../docs/figs/temperature_p51D.png
+cp resultsp51D1e14/Kn.png ../docs/figs/Kn_p51D1e14.png
+cp resultsp51D1e14/hflux.png ../docs/figs/hflux_p51D1e14.png
+cp resultsp51D1e13/Kn.png ../docs/figs/Kn_p51D1e13.png
+cp resultsp51D1e13/hflux.png ../docs/figs/hflux_p51D1e13.png
+cp resultsp51D1e12/Kn.png ../docs/figs/Kn_p51D1e12.png
+cp resultsp51D1e12/hflux.png ../docs/figs/hflux_p51D1e12.png
+cp resultsp51D1e11/Kn.png ../docs/figs/Kn_p51D1e11.png
+cp resultsp51D1e11/hflux.png ../docs/figs/hflux_p51D1e11.png
+cp resultsp51D1e10/Kn.png ../docs/figs/Kn_p51D1e10.png
+cp resultsp51D1e10/hflux.png ../docs/figs/hflux_p51D1e10.png
+cp resultsp51D1e9/Kn.png ../docs/figs/Kn_p51D1e9.png
+cp resultsp51D1e9/hflux.png ../docs/figs/hflux_p51D1e9.png
+cp resultsp51D1e8/Kn.png ../docs/figs/Kn_p51D1e8.png
+cp resultsp51D1e8/hflux.png ../docs/figs/hflux_p51D1e8.png
+cp resultsp51D5e7/Kn.png ../docs/figs/Kn_p51D5e7.png
+cp resultsp51D5e7/hflux.png ../docs/figs/hflux_p51D5e7.png
+# problem 6 in 1D
+# generate figs locally
+cd resultsp61D1e14
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D1e13
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D1e12
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D1e11
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D1e10
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D1e9
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D1e8
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp61D5e7
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ..
+# copy figs
+cp resultsp61D1e14/density.png ../docs/figs/density_p61D.png
+cp resultsp61D1e14/temperature.png ../docs/figs/temperature_p61D.png
+cp resultsp61D1e14/Kn.png ../docs/figs/Kn_p61D1e14.png
+cp resultsp61D1e14/hflux.png ../docs/figs/hflux_p61D1e14.png
+cp resultsp61D1e13/Kn.png ../docs/figs/Kn_p61D1e13.png
+cp resultsp61D1e13/hflux.png ../docs/figs/hflux_p61D1e13.png
+cp resultsp61D1e12/Kn.png ../docs/figs/Kn_p61D1e12.png
+cp resultsp61D1e12/hflux.png ../docs/figs/hflux_p61D1e12.png
+cp resultsp61D1e11/Kn.png ../docs/figs/Kn_p61D1e11.png
+cp resultsp61D1e11/hflux.png ../docs/figs/hflux_p61D1e11.png
+cp resultsp61D1e10/Kn.png ../docs/figs/Kn_p61D1e10.png
+cp resultsp61D1e10/hflux.png ../docs/figs/hflux_p61D1e10.png
+cp resultsp61D1e9/Kn.png ../docs/figs/Kn_p61D1e9.png
+cp resultsp61D1e9/hflux.png ../docs/figs/hflux_p61D1e9.png
+cp resultsp61D1e8/Kn.png ../docs/figs/Kn_p61D1e8.png
+cp resultsp61D1e8/hflux.png ../docs/figs/hflux_p61D1e8.png
+cp resultsp61D5e7/Kn.png ../docs/figs/Kn_p61D5e7.png
+cp resultsp61D5e7/hflux.png ../docs/figs/hflux_p61D5e7.png
+# problem 7 in 1D
+# generate figs locally
+cd resultsp71D1e14
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D1e13
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D1e12
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D1e11
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D1e10
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D1e9
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D1e8
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ../resultsp71D5e7
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_1Dfigures.glvs
+cd ..
+# copy figs
+cp resultsp71D1e14/density.png ../docs/figs/density_p71D.png
+cp resultsp71D1e14/temperature.png ../docs/figs/temperature_p71D.png
+cp resultsp71D1e14/Kn.png ../docs/figs/Kn_p71D1e14.png
+cp resultsp71D1e14/hflux.png ../docs/figs/hflux_p71D1e14.png
+cp resultsp71D1e13/Kn.png ../docs/figs/Kn_p71D1e13.png
+cp resultsp71D1e13/hflux.png ../docs/figs/hflux_p71D1e13.png
+cp resultsp71D1e12/Kn.png ../docs/figs/Kn_p71D1e12.png
+cp resultsp71D1e12/hflux.png ../docs/figs/hflux_p71D1e12.png
+cp resultsp71D1e11/Kn.png ../docs/figs/Kn_p71D1e11.png
+cp resultsp71D1e11/hflux.png ../docs/figs/hflux_p71D1e11.png
+cp resultsp71D1e10/Kn.png ../docs/figs/Kn_p71D1e10.png
+cp resultsp71D1e10/hflux.png ../docs/figs/hflux_p71D1e10.png
+cp resultsp71D1e9/Kn.png ../docs/figs/Kn_p71D1e9.png
+cp resultsp71D1e9/hflux.png ../docs/figs/hflux_p71D1e9.png
+cp resultsp71D1e8/Kn.png ../docs/figs/Kn_p71D1e8.png
+cp resultsp71D1e8/hflux.png ../docs/figs/hflux_p71D1e8.png
+cp resultsp71D5e7/Kn.png ../docs/figs/Kn_p71D5e7.png
+cp resultsp71D5e7/hflux.png ../docs/figs/hflux_p71D5e7.png
+# problem 7 in 2D
+# generate figs locally
+cd resultsp72D1e12
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_2Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_2Dfigures.glvs
+cd ../resultsp72D1e11
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_2Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_2Dfigures.glvs
+cd ../resultsp72D1e10
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_2Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_2Dfigures.glvs
+cd ../resultsp72D1e9
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_2Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_2Dfigures.glvs
+cd ../resultsp72D1e8
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_2Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_2Dfigures.glvs
+cd ..
+# copy figs
+cp resultsp72D1e12/density.png ../docs/figs/density_p72D.png
+cp resultsp72D1e12/temperature.png ../docs/figs/temperature_p72D.png
+cp resultsp72D1e12/Kn.png ../docs/figs/Kn_p72D1e12.png
+cp resultsp72D1e12/hflux.png ../docs/figs/hflux_p72D1e12.png
+cp resultsp72D1e11/Kn.png ../docs/figs/Kn_p72D1e11.png
+cp resultsp72D1e11/hflux.png ../docs/figs/hflux_p72D1e11.png
+cp resultsp72D1e10/Kn.png ../docs/figs/Kn_p72D1e10.png
+cp resultsp72D1e10/hflux.png ../docs/figs/hflux_p72D1e10.png
+cp resultsp72D1e9/Kn.png ../docs/figs/Kn_p72D1e9.png
+cp resultsp72D1e9/hflux.png ../docs/figs/hflux_p72D1e9.png
+cp resultsp72D1e8/Kn.png ../docs/figs/Kn_p72D1e8.png
+cp resultsp72D1e8/hflux.png ../docs/figs/hflux_p72D1e8.png
+# problem 7 in 3D
+# generate figs locally
+cd resultsp73D1e12
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_3Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_3Dfigures.glvs
+cd ../resultsp73D1e11
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_3Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_3Dfigures.glvs
+cd ../resultsp73D1e10
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_3Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_3Dfigures.glvs
+cd ../resultsp73D1e9
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_3Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_3Dfigures.glvs
+cd ../resultsp73D1e8
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_3Dfigures.glvs
+../../../glvis-3.3/glvis -run ../create_parallel_M1Philippe_flux_3Dfigures.glvs
+cd ..
+# copy figs
+cp resultsp73D1e12/density.png ../docs/figs/density_p73D.png
+cp resultsp73D1e12/temperature.png ../docs/figs/temperature_p73D.png
+cp resultsp73D1e12/Kn.png ../docs/figs/Kn_p73D1e12.png
+cp resultsp73D1e12/hflux.png ../docs/figs/hflux_p73D1e12.png
+cp resultsp73D1e11/Kn.png ../docs/figs/Kn_p73D1e11.png
+cp resultsp73D1e11/hflux.png ../docs/figs/hflux_p73D1e11.png
+cp resultsp73D1e10/Kn.png ../docs/figs/Kn_p73D1e10.png
+cp resultsp73D1e10/hflux.png ../docs/figs/hflux_p73D1e10.png
+cp resultsp73D1e9/Kn.png ../docs/figs/Kn_p73D1e9.png
+cp resultsp73D1e9/hflux.png ../docs/figs/hflux_p73D1e9.png
+cp resultsp73D1e8/Kn.png ../docs/figs/Kn_p73D1e8.png
+cp resultsp73D1e8/hflux.png ../docs/figs/hflux_p73D1e8.png
